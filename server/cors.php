2017023dd233666998ccd0a465dba9a492b1af96
Marty0011 <saltycat45@gmail.com>
2024-06-09 23:17:42 -0400

    first commit

---
+++ server/cors.php
@@ -0,0 +1,6 @@
+<?php
+// Allow cross origin requests
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+?>
